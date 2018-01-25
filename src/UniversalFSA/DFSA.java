0986cf81702f58b71dfe3a98eacb3b010ed5e0e1
Kenneth Ung <kennethleeung@gmail.com>
2018-01-25 01:27:05 -0800

    Final Version of the Universal DFSA, haven't done any reorganization to make the code look cleaner yet

--- src/UniversalFSA/DFSA.java
+++ src/UniversalFSA/DFSA.java
@@ -2,25 +2,37 @@ package UniversalFSA;
 
 public class DFSA {
 
+
+
     private String[] alphabet;
     private boolean[] FINAL;
     private int[] states;
     private int[][] transitionFunction;
     private int numOfStates;
 
+
     public DFSA() {
     }
 
     public boolean checkAccept(String input) {
         boolean answer = false;
-        int initialState = 0;
-        int nextState = 0; //Representation of the current state you are in
-        for(int i = 0; i < input.length(); i ++) {
-            for(int j = 0; j < alphabet.length; j ++) {
-                if(input.charAt(i) == alphabet[j].charAt(0)) {
-                    nextState = transitionFunction[nextState][j];
-                    answer = FINAL[nextState];
+        int nextState = 0; 
+        if(input.length() <= 0) {
+            nextState = transitionFunction[nextState][0];
+            answer = FINAL[nextState];
+        }
+        else {
+            for (int i = 0; i < input.length(); i++) {
+                for (int j = 0; j < alphabet.length; j++) {
+                    if (input.charAt(i) == alphabet[j].charAt(0)) {
+                        nextState = transitionFunction[nextState][j];
+                        if(nextState == -1) {
+                            return false;
+                        }
 
+                        answer = FINAL[nextState];
+
+                    }
                 }
             }
         }
@@ -61,9 +73,19 @@ public class DFSA {
 
     }
 
-    public void setTransition(int originState, int givenAlphabet, int nextState) {
+    public void setTransition(int originState, String givenAlphabet, int nextState) {
 
-        transitionFunction[originState][givenAlphabet] = nextState;
+        int alphabetIndex = 0;
+
+        for(int i = 0; i < alphabet.length; i++) {
+            if(alphabet[i].compareTo(givenAlphabet) == 0) {
+                alphabetIndex = i;
+                break;
+            }
+
+        }
+
+        transitionFunction[originState][alphabetIndex] = nextState;
 
     }
 
@@ -75,6 +97,10 @@ public class DFSA {
             }
 
         }
+    }
+
+    public void printTransitionFunction(int state, int alphabet) {
+        System.out.println(state + " " + alphabet + " " + transitionFunction[state][alphabet]);
     }
 
 
--- src/UniversalFSA/Driver.java
+++ src/UniversalFSA/Driver.java
@@ -7,10 +7,11 @@ public class Driver {
 
     public static void main (String[] args) {
 
-        String[] transitions;
-        String answer = "";
+        int counter = 0; // Will be used for a print statement that keeps track of what # automaton we are on
 
-        DFSA universalDFSA = new DFSA();
+        String[] transitions; // Constantly changes, used as a helper variable to implement the transition into our transition table
+
+        DFSA universalDFSA = new DFSA(); // Our Universal DFSA Object, which does all of the computing in our program
 
         /* Representation of the text file that will be inputted into the Program */
         String fileName = "input.txt";
@@ -20,70 +21,59 @@ public class Driver {
 
             /* The scanner (sc) is what we are going to be using to read the input.txt file that holds our DFSAs */
             Scanner sc = new Scanner(new File(fileName));
-            int numOfStates = sc.nextInt();
-            universalDFSA.setStates(numOfStates);
-            sc.nextLine();
-            String parser = sc.nextLine();
-            String[] arrOfFinal = parser.split(" , ");
-            int[] fStates = new int[arrOfFinal.length];
-            for(int i = 0; i < arrOfFinal.length;i++) {
-                fStates[i] = Integer.parseInt(arrOfFinal[i]);
-            }
-            for(int i = 0; i < fStates.length; i ++) {
-                universalDFSA.setFinalStates(fStates[i]);
-            }
-            parser = sc.nextLine();
-            String[] arrOfAlphabet = parser.split(" ");
-            universalDFSA.setAlphabet(arrOfAlphabet);
-            universalDFSA.printAlphabet();
-            universalDFSA.setTransitionFunction(numOfStates, arrOfAlphabet.length);
-            while(sc.hasNext()) {
+
+            while (sc.hasNext()) {
+                System.out.println("Finite State Automaton #" + ++counter);
+                int numOfStates = sc.nextInt();
+                System.out.println("(1) number of states: " + numOfStates);
+                universalDFSA.setStates(numOfStates);
+                sc.nextLine();
+                String parser = sc.nextLine();
+                System.out.println("(2) final states: " + parser);
+                String[] arrOfFinal = parser.split(",");
+                int[] finalStates = new int[arrOfFinal.length];
+                for (int i = 0; i < arrOfFinal.length; i++) { // This for loop is responsible for assigning the final states
+                    finalStates[i] = Integer.parseInt(arrOfFinal[i]);
+                }
+                for (int i = 0; i < finalStates.length; i++) { // This loop sets final States of current DFSA
+                    universalDFSA.setFinalStates(finalStates[i]);
+                }
                 parser = sc.nextLine();
-                System.out.println(parser);
-                transitions = parser.split(" ");
-                if(transitions[0].compareTo("(") == 0) {
+                System.out.println("(3) alphabets: " + parser);
+                String[] arrOfAlphabet = parser.split(" ");
+                universalDFSA.setAlphabet(arrOfAlphabet);
+                universalDFSA.setTransitionFunction(numOfStates, arrOfAlphabet.length);
+                System.out.println("(4) transitions: ");
+                parser = sc.nextLine();
+                while (parser.startsWith("(")) {
+                    transitions = parser.split(" ");
                     int originState = Integer.parseInt(transitions[1]);
-                    System.out.println(originState);
-                    int givenAlphabet = Integer.parseInt(transitions[2]);
+                    String givenAlphabet = transitions[2];
                     String nextState = transitions[3];
                     int nextS;
-                    if(nextState == " "){
+                    if (nextState == " ") {
                         nextS = -1;
+                    } else {
+                        nextS = Integer.parseInt(nextState);
                     }
-                    else {
-                        nextS = Integer.parseInt(transitions[3]);
-                    }
-
-                    universalDFSA.setTransition(originState,givenAlphabet,nextS);
+                    universalDFSA.setTransition(originState, givenAlphabet, nextS);
+                    System.out.println("     " + originState + " " + givenAlphabet + " " + nextS);
+                    parser = sc.nextLine();
                 }
-                else {
-                    /*  */
+                System.out.println("(5) Strings: ");
+                while (!parser.startsWith("/") && sc.hasNext()) {
+                    System.out.print("       " + parser);
                     boolean accepted = universalDFSA.checkAccept(parser);
-                    if(accepted)
-                        System.out.println("Accepted");
+                    if (accepted)
+                        System.out.println(" Accepted");
                     else
-                        System.out.println("Rejected");
-                    answer += parser + " ";
+                        System.out.println(" Rejected");
 
+                    parser = sc.nextLine();
                 }
-
+                System.out.println("\n\n\n");
             }
-            System.out.print(answer);
-
-
         }
-
-
-//        try {
-//            FileReader fileReader = new FileReader(fileName);
-//            BufferedReader bufferedReader = new BufferedReader(fileReader);
-//            while((line = bufferedReader.readLine()) != null) {
-//                System.out.println(line);
-//                char[] test = line.toCharArray();
-//
-//            }
-//            bufferedReader.close();
-//        }
         catch(FileNotFoundException notFound) {
             System.out.println("Unable to open file '" + fileName + "'");
         }
